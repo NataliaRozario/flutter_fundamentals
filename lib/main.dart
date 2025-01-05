ee0f304c2a64796e79aa7f69d9e401eef17af3cb
NataliaRozario <natyraissa137@gmail.com>
2025-01-05 09:27:44 -0300

    new widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  void userTapped() {
+    print("User tapped!");
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.cyan[300],
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.cyan[700],
-              borderRadius: BorderRadius.circular(20),
+          child: GestureDetector(
+            onTap: userTapped,
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.cyan[200],
+              child: Center(child: Text("Tap me!")),
             ),
-            padding: EdgeInsets.all(25),
-            child: 
           ),
         ),
       ),
@@ -40,3 +42,118 @@ Text(
   ),
 );
 */
+
+/*
+Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.cyan[700],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Icon(
+              Icons.favorite,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ),
+ */
+
+/* 
+appBar: AppBar(
+          title: Text(
+            "My App Bar",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.cyan[700],
+          leading: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ))
+          ],
+        ),
+*/
+
+/*
+   ListView(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // scrollDirection: Axis.vertical,
+          children: [
+            Container(
+              height: 350,
+              width: 350,
+              color: Colors.cyan[700],
+            ),
+            Container(
+              height: 350,
+              width: 350,
+              color: Colors.cyan[500],
+            ),
+            Container(
+              height: 350,
+              width: 350,
+              color: Colors.cyan[300],
+            ),
+          ],
+        ),
+ */
+
+/*
+  List names = ["Mitch", "Sharon", "Vince"];
+
+ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(names[index]),
+          ),
+        ),
+*/
+
+/*
+  GridView.builder(
+          itemCount: 64,
+          gridDelegate:
+          // how many you want in each row
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemBuilder: (context, index) => Container(
+            color: Colors.cyan[700],
+            margin: EdgeInsets.all(2),
+          ),
+        ),
+ */
+
+/*
+Stack(
+          alignment: Alignment.bottomRight,
+          children: [
+            Container(
+              height: 300,
+              width: 300,
+              color: Colors.cyan[700],
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.cyan[500],
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.cyan[300],
+            ),
+          ],
+        ),
+ */
